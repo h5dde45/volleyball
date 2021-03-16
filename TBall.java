23c7094ac1360ca615e399e7b182bfa2284b08b8
h5dde45 <tmvf@yandex.ru>
2021-03-16 15:29:18 +0300

    Update TBall.java

--- TBall.java
+++ TBall.java
@@ -1,1 +1,48 @@
+package com.n7;
+
+public class TBall {
+    private float x, y;
+    private float dx, dy;
+    private float r;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getDx() {
+        return dx;
+    }
+
+    public void setDx(float dx) {
+        this.dx = dx;
+    }
+
+    public float getDy() {
+        return dy;
+    }
+
+    public void setDy(float dy) {
+        this.dy = dy;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
+}
 
